a78d52576c1a44bb404822ac2e8622587756afb6
shreya25bai10326-pixel <shreya.25bai10326@vitbhopal.ac.in>
2025-11-22 18:56:13 +0530

    Update calculator.py

--- BILL-SPLITTING-CALCULATOR/calculator.py
+++ BILL-SPLITTING-CALCULATOR/calculator.py
@@ -1,1 +1,16 @@
+# ==== calculator.py functions ====
 
+def calculate_split(total_bill, participants):
+  """
+  Calculate how much each participant owes or should receivw.
+  Positive means they get money back, negative means they owe money.
+  """
+  n = len(participants)
+  fair_share = total_bill / n
+  split_summary = {}
+
+  for name, contribution in participants.items():
+    split_summary[name] = contribution - fair_share
+
+  return split_summary
+  
