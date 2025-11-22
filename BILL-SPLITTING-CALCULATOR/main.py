29e4e89a02f15b69401abbea73d30bb255090684
shreya25bai10326-pixel <shreya.25bai10326@vitbhopal.ac.in>
2025-11-22 19:03:00 +0530

    Implement main function for bill splitting calculator

--- BILL-SPLITTING-CALCULATOR/main.py
+++ BILL-SPLITTING-CALCULATOR/main.py
@@ -1,1 +1,20 @@
+# ==== main.py code ====
+def main():
+  print("=== Bill Splitting Calculator ===\n")
 
+  participants = get_participants()
+
+  total_bill = float(input("\nEnter the total bill amount: "))
+
+  split_summary = calculate_split(total_bill, participants)
+
+  print("\n--- Bill Split Summary ---")
+  for name, amount in split_summary.items():
+    if amount >=0:
+      print(f"{name} should recieve ₹{amount:.2f}")
+    else:
+      print(f"{name} oews ₹{-amount:.2f}")
+
+if __name__ == "__main__":
+  main()
+      
