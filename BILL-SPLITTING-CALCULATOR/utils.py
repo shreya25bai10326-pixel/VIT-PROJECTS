45a90da687ede3fadc345a8a9227ffd92ee095ab
shreya25bai10326-pixel <shreya.25bai10326@vitbhopal.ac.in>
2025-11-22 18:34:54 +0530

    Implement get_participants function
    Added a function to collect participant names and contributions.

--- BILL-SPLITTING-CALCULATOR/utils.py
+++ BILL-SPLITTING-CALCULATOR/utils.py
@@ -1,1 +1,17 @@
+#==== utils.py functions ====
+
+def get_participants():
+  """
+  Collect participant name and their contributions.
+  Returns a dictionary of participants with contibutions.
+  """
+  participants = {}
+  n = int(input("Enter the number of participants: "))
+
+  for i in range(n):
+    name = input(f"Enter the name of participant {i+1}: ")
+    contribution = float(input(f"Enter contribution by {name}:
+    participants[name] = contribution
+
+  return participants
 
